1b6b849c7347ad157b781664a5f58d23407125bc
Burak Bahar <burakthebalrogslayer@gmail.com>
2024-07-15 22:42:26 +0300

    working Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
 
     public Transform player; // Reference to the camera's transform
-    public float jumpForce = 3f; // Jump force
+    public float jumpForce = 10f; // Jump force
     public float groundDistance = 0.2f; // Distance to check for the ground
     public LayerMask groundMask; // Layer mask to specify what is ground
     [SerializeField] public float speed = 3f; // Movement speed
@@ -14,19 +14,15 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb; // Reference to the Rigidbody component
     private bool isGrounded; // Is the player grounded
 
-
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+        //isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
         print(isGrounded);
     
         float horizontal = Input.GetAxis("Horizontal");
@@ -46,10 +42,23 @@ public class PlayerController : MonoBehaviour
             //transform.Translate(moveDir.normalized * speed, Space.World);
             rb.MovePosition(transform.position + moveDir.normalized * speed );
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            if(Input.GetKeyDown(KeyCode.W)){
+                Vector3 frontJump = transform.forward + Vector3.up;
+                rb.AddForce(frontJump * jumpForce, ForceMode.Impulse);
+            }
+            else{
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            }
+            isGrounded =false;
         }
 
     }
@@ -61,6 +70,7 @@ public class PlayerController : MonoBehaviour
     }
     void OnCollisionStay(Collision collision) {
         if (collision.gameObject.CompareTag("Terrain")){
+            isGrounded=true;
         }
     }
 }
