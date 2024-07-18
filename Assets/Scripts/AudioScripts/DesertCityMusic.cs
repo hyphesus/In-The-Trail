2c4a154eb30023fa40b34cb802b815705637a0d2
Burak Bahar <burakthebalrogslayer@gmail.com>
2024-07-18 16:31:12 +0300

    desert stop menu and music is done

--- Assets/Scripts/AudioScripts/DesertCityMusic.cs
+++ Assets/Scripts/AudioScripts/DesertCityMusic.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DesertCityMusic : MonoBehaviour
 {
@@ -6,23 +7,24 @@ public class DesertCityMusic : MonoBehaviour
     public float delay = 5f; // Delay time in seconds before the song plays again
     public float fadeDuration = 2f; // Duration of the fade in/out effect in seconds
 
-    private AudioSource audioSource;
+    public AudioSource audioSource;
     private bool isWaiting = false;
     private bool isFadingIn = true;
     private bool isFadingOut = false;
     private float waitTime = 0f;
     private float initialVolume;
+    public Scrollbar scrollbar;
 
     void Start()
     {
         // Add an AudioSource component to the GameObject this script is attached to
         audioSource = gameObject.AddComponent<AudioSource>();
-
+        
         // Assign the AudioClip to the AudioSource
         audioSource.clip = gameMusic;
-
+        
         // Set initial volume and start with fade-in
-        initialVolume = audioSource.volume;
+        initialVolume = scrollbar.value;
         audioSource.volume = 0;
 
         // Play the audio
--- Assets/Scripts/UI/StopUI.cs
+++ Assets/Scripts/UI/StopUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class StopUI : MonoBehaviour
@@ -10,10 +11,16 @@ public class StopUI : MonoBehaviour
     public GameObject gameState; // player.ispaused
     // Start is called before the first frame update
     public void ContinueGame(){
+        print("continue");
         gameState.GetComponent<PlayerController>().Resume();
     }
 
     public void ChangeAudio(){
-        sceneaudio.GetComponent<MusicPlayer>().audioSource.volume = scrollbar.value*0.4f;
+        if (sceneaudio.GetComponent<MusicPlayer>() != null){
+            sceneaudio.GetComponent<MusicPlayer>().audioSource.volume = scrollbar.value*0.4f;
+        }
+        else if(sceneaudio.GetComponent<DesertCityMusic>() != null){
+            sceneaudio.GetComponent<DesertCityMusic>().audioSource.volume = scrollbar.value*0.4f;
+        }
     }
 }
